e20cc888e2677c5f8b31b1251dbda2ffebd32c40
Griefed <griefed@griefed.de>
2023-11-10 13:23:35 +0100

    Make ModStairs constructor public

--- common/src/main/java/de/griefed/addemall/block/ModStairs.java
+++ common/src/main/java/de/griefed/addemall/block/ModStairs.java
@@ -4,7 +4,7 @@ import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class ModStairs extends StairBlock {
-    protected ModStairs(BlockState blockState, Properties properties) {
+    public ModStairs(BlockState blockState, Properties properties) {
         super(blockState, properties);
     }
 }
